5fca0a879b08aec3bf52fdb79a2afa6917611d4d
MatiasBarrera <matias.barrera95@gmail.com>
2022-01-18 10:01:06 -0300

    fix: se resuelven problemas

--- pages/api/product/products.js
+++ pages/api/product/products.js
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function getProducts(req, res) {
+
   const products = await prisma.product.findMany()
   res.status(200).json(products)
 }

c915787e08b6a6016db9efe07060c5d36fffdcd1
MatiasBarrera <matias.barrera95@gmail.com>
2022-02-02 17:05:01 -0300

    build: env en next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images: {
     domains: ['losangeles.comes.cl', 'imgur.com', 'comeschile.cl', 'cs-stores-images.s3.amazonaws.com'],
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_IMAGES_PATH: process.env.MY_ENV_VAR,
+    API_URL: process.env.API_URL,
+  },
 }

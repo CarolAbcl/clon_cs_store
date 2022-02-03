d021a73c939dcb9d9509267dcadee21defdf3ab0
katherine-valcarce <katherine.v@live.cl>
2022-02-03 12:45:43 -0300

    feature: se agrega pagina de carrito version desktop

---
+++ components/ProductCardDesktop.js
@@ -0,0 +1,116 @@
+import { Icon } from '@material-ui/core'
+import Image from 'next/image'
+import Link from 'next/link'
+import CardPrice from './atoms/CardPrice'
+import QtyAddCart from './QtyAddCart'
+import { useDispatch } from 'react-redux'
+import { deleteItemCart } from '../store/actions/cartAction'
+
+function ProductCardDesktop({ product, inCart }) {
+  const price_package = '$' + new Intl.NumberFormat('de-DE').format(product.price_package)
+  const subTotal_price = inCart ? '$' + new Intl.NumberFormat('de-DE').format(product.qty * product.price_package) : 0
+  return (
+    <>
+      <div className="ProductCardDesktop">
+        <div className="imgContainer">
+          <Link href={`/product/${product.slug}`}>
+            <a>
+              <Image
+                src={`${process.env.NEXT_PUBLIC_IMAGES_PATH}/${
+                  product.image[0] ? product.image[0].file_image : 'imagen_no_disponible.jpg'
+                }`}
+                width={60}
+                height={70}
+                alt="Imagen producto"></Image>
+            </a>
+          </Link>
+        </div>
+        <div className="containerInfoProduct">
+          <div className="ProductName">
+            <Link href={`/product/${product.slug}`}>
+              <a>
+                <h2>{product.name}</h2>
+              </a>
+            </Link>
+          </div>
+          <div className="QtyAddCart">
+            <QtyAddCart product={product} />
+          </div>
+          <div className="CardPrice">
+            <CardPrice PriceProduct={price_package} inCart={inCart} sizeFont={'1rem'} />
+          </div>
+          <div>
+            <p className="subtotal">
+              Subtotal <span className="secondary subtotal">{subTotal_price}</span>
+            </p>
+          </div>
+        </div>
+        <div className="IconDelete">
+          <Icon
+            style={{
+              color: 'var(--gray)',
+              width: '100%',
+              textAlign: 'right',
+              cursor: 'pointer',
+            }}
+            onClick={() => dispatch(deleteItemCart(product))}>
+            delete
+          </Icon>
+        </div>
+      </div>
+      <style jsx>{`
+        .ProductCardDesktop {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          background-color: var(--light);
+          box-shadow: 3px 2px 12px -5px rgba(0, 0, 0, 0.5);
+          border-radius: 8px;
+          align-items: center;
+          flex-shrink: 1;
+          padding: 0.5rem;
+          margin: 0.8rem 0;
+          min-height: '15rem';
+          position: relative;
+          justify-content: space-between;
+          width: 67%;
+        }
+        .containerInfoProduct {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-between;
+          width: 85%;
+        }
+        .imgContainer {
+          width: 8%;
+        }
+        .IconDelete {
+          width: 5%;
+        }
+        .ProductName {
+          width: 18rem;
+        }
+        .subtotal {
+          display: flex;
+          flex-direction: column;
+          font-weight: bold;
+          text-align: center;
+          width: 5rem;
+        }
+        .QtyAddCart {
+          width: 7rem;
+        }
+        .CardPrice {
+          width: 9rem;
+        }
+
+        h2 {
+          font-size: 1rem;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default ProductCardDesktop
--- pages/carrito.js
+++ pages/carrito.js
@@ -5,6 +5,7 @@ import CardsGroup from '../components/CardsGroup'
 import { Button, ButtonSecondary } from '../components/atoms/buttons'
 import { Icon } from '@material-ui/core'
 import Link from 'next/link'
+import ProductCardDesktop from '../components/ProductCardDesktop'
 
 function Carrito() {
   const cart = useSelector((state) => state.cart)
@@ -17,50 +18,85 @@ function Carrito() {
           <SearchBar />
         </div>
         <hr />
-        {cart.length !== 0 ? (
-          <div className="carrito">
-            <CardsGroup inCart>
-              {cart.map((product) => (
-                <ProductCard key={product.ID_product} product={product} inCart />
-              ))}
-            </CardsGroup>
-            <div className="total">
-              <p className="font125">
-                Total: <span className="font125 secondary">{total}</span>
-              </p>
-            </div>
-            <Link href="/catalogo" passHref>
-              <ButtonSecondary value="Seguir comprando" fontSize="1rem" />
-            </Link>
-            <div className="actionButton">
-              <Button value="cancelar" color="var(--secondary)" />
-              <Button value="confirmar" color="var(--primary)" />
-            </div>
-            <div className="actionButton share">
-              <div className="center">
-                <Icon style={{ color: '#7B61FF', fontSize: '2.5rem' }}>share</Icon>
-                <p>Compartir mi carrito</p>
+        <div className="containerCart">
+          {cart.length !== 0 ? (
+            <>
+              <div className="containerCardMobile">
+                <CardsGroup inCart>
+                  {cart.map((product) => (
+                    <ProductCard key={product.ID_product} product={product} inCart />
+                  ))}
+                </CardsGroup>
               </div>
-              <div className="center">
-                <Icon style={{ color: '#7B61FF', fontSize: '2.5rem' }}>print</Icon>
-                <p>Imprimir carrito</p>
+              <div className="containerCardDesktop">
+                {cart.map((product) => (
+                  <ProductCardDesktop key={product.ID_product} product={product} inCart />
+                ))}
               </div>
+              <div className="containerTotalMobile">
+                <div className="total">
+                  <p className="font125">
+                    Total: <span className="font125 secondary">{total}</span>
+                  </p>
+                </div>
+                <Link href="/catalogo" passHref>
+                  <ButtonSecondary value="Seguir comprando" fontSize="1rem" />
+                </Link>
+                <div className="actionButton">
+                  <Button value="cancelar" color="var(--secondary)" />
+                  <Button value="confirmar" color="var(--primary)" />
+                </div>
+                <div className="actionButton share">
+                  <div className="center">
+                    <Icon style={{ color: '#7B61FF', fontSize: '2.5rem' }}>share</Icon>
+                    <p>Compartir mi carrito</p>
+                  </div>
+                  <div className="center">
+                    <Icon style={{ color: '#7B61FF', fontSize: '2.5rem' }}>print</Icon>
+                    <p>Imprimir carrito</p>
+                  </div>
+                </div>
+              </div>
+              <div className="containerTotalDesktop">
+                <div className="total">
+                  <p className="font125">
+                    Total: <span className="font125 secondary">{total}</span>
+                  </p>
+                </div>
+                <Link href="/catalogo" passHref>
+                  <ButtonSecondary value="Seguir comprando" fontSize="1rem" />
+                </Link>
+                <div className="actionButton">
+                  <Button value="cancelar" color="var(--secondary)" />
+                  <Button value="confirmar" color="var(--primary)" />
+                </div>
+                <div className="actionButton share">
+                  <div className="center">
+                    <Icon style={{ color: '#7B61FF', fontSize: '2.5rem' }}>share</Icon>
+                    <p>Compartir mi carrito</p>
+                  </div>
+                  <div className="center">
+                    <Icon style={{ color: '#7B61FF', fontSize: '2.5rem' }}>print</Icon>
+                    <p>Imprimir carrito</p>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="containerEmptyCart center">
+              <Icon style={{ color: 'var(--light-gray', fontSize: '80px', marginTop: '3rem' }}>shopping_cart</Icon>
+              <h2>
+                No has agregado productos al carrito. Ingresa al{' '}
+                <Link href="/catalogo">
+                  <a className="primary" style={{ fontSize: 'inherit' }}>
+                    catálogo
+                  </a>
+                </Link>{' '}
+                para agregar.
+              </h2>
             </div>
-          </div>
-        ) : (
-          <div className="center">
-            <Icon style={{ color: 'var(--light-gray', fontSize: '80px', marginTop: '3rem' }}>shopping_cart</Icon>
-            <h2>
-              No has agregado productos al carrito. Ingresa al{' '}
-              <Link href="/catalogo">
-                <a className="primary" style={{ fontSize: 'inherit' }}>
-                  catálogo
-                </a>
-              </Link>{' '}
-              para agregar.
-            </h2>
-          </div>
-        )}
+          )}
+        </div>
       </div>
       <style jsx>
         {`
@@ -96,10 +132,13 @@ function Carrito() {
             font-size: 1.25rem !important;
             margin: 0;
           }
-          .carrito {
+          .containerCardMobile {
             display: flex;
             flex-direction: column;
             align-items: center;
+          }
+          .containerCardDesktop {
+            display: none;
           }
           .actionButton {
             display: flex;
@@ -115,6 +154,49 @@ function Carrito() {
           }
           .center {
             text-align: center;
+          }
+          .containerTotalMobile {
+            display: block;
+          }
+          .containerTotalDesktop {
+            display: none;
+          }
+          @media (min-width: 800px) {
+            .containerCart {
+              display: flex;
+              flex-direction: row;
+              justify-content: center;
+            }
+            .containerCardMobile {
+              display: none;
+            }
+            .containerTotalMobile {
+              display: none;
+            }
+            .containerCardDesktop {
+              display: flex;
+              flex: 3;
+              flex-direction: column;
+              align-items: flex-start;
+              flex-wrap: nowrap;
+              justify-content: space-between;
+            }
+            .containerTotalDesktop {
+              display: flex;
+              flex: 1;
+              flex-direction: column;
+              align-items: center;
+              flex-wrap: nowrap;
+              justify-content: space-between;
+              background-color: var(--light);
+              box-shadow: 3px 2px 12px -5px rgba(0, 0, 0, 0.5);
+              border-radius: 8px;
+              width: 30%;
+              height: fit-content;
+              padding: 2rem;
+              position: fixed;
+              right: 1rem;
+            }
           }
         `}
       </style>

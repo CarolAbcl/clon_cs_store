bb5b1e15e971ac30f475549d0340856f64df55f5
Joaquín Ignacio Ossandón Gómez <58451051+Gh0stRaccoon@users.noreply.github.com>
2022-02-10 11:43:42 -0300

    fix: Se corrige problema con cantidad minima en Kg, alert, test scroll, imagenes cuando es main y roundButton

--- helpers/purchaseFormat.js
+++ helpers/purchaseFormat.js
@@ -1,6 +1,9 @@
 export default function purchaseFormat(qty, type){
-  if(type !== 'monto'){
+  if(type !== 'monto' && type !== 'kilos'){
     return qty == 1 ? `${qty} ${type.slice(0,-1)}` : `${qty} ${type}`
+  } else if(type == 'kilos'){
+    const qtyKg = qty/1000
+    return qtyKg == 1 ? `${qtyKg} ${type.slice(0,-1)}` : `${qtyKg} ${type}`
   } else {
     return `$${new Intl.NumberFormat('de-DE').format(qty)}`
   }

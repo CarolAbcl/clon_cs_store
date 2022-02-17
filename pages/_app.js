8adc18bc9274fe795da82531d3dd5f658a3f7cda
katherine-valcarce <katherine.v@live.cl>
2022-02-17 14:55:43 -0300

    fix: si la pagina es distinta a catalogo o product reinicia el estado returnCatalogue

--- pages/_app.js
+++ pages/_app.js
@@ -10,15 +10,15 @@ import { useEffect, useState } from 'react'
 function MyApp({ Component, pageProps }) {
   //Estado que guarda los productos mostrados y posicion del scroll antes de cliquear una tarjeta
   const [returnCatalogue, setReturnCatalogue] = useState({ loadedProducts: null, positionScroll: 0 })
+  //hook que obtiene la pagina dondre estamos para manejar el estado de los productos guardados
   useEffect(() => {
-    // const { location } = window
-    // const pathName = location.pathname.slice(0, 9)
-    // setTimeout(() => {
-    //   pathName === ('/catalogo' || '/product/')
-    //     ? console.log('no se borra', pathName)
-    //     : console.log('se borra estado', pathName)
-    // }, 3000)
-  })
+    const { location } = window
+    const pathName = location.pathname.slice(0, 9)
+    if (pathName === '/product/' || pathName === '/catalogo') {
+    } else {
+      setReturnCatalogue({ loadedProducts: null, positionScroll: 0 })
+    }
+  }, [])
   return (
     <>
       <Head>

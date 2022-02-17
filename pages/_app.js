aacd7e9ebde4f7e8d84c6a01fda05db5226ce582
katherine-valcarce <katherine.v@live.cl>
2022-02-17 16:01:56 -0300

    refactor: se elimina useEffect de app y se llama a useRouter

--- pages/_app.js
+++ pages/_app.js
@@ -5,20 +5,18 @@ import { Provider } from 'react-redux'
 import { createWrapper } from 'next-redux-wrapper'
 import store from '../store/store'
 import Alert from '../components/Alert'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
   //Estado que guarda los productos mostrados y posicion del scroll antes de cliquear una tarjeta
   const [returnCatalogue, setReturnCatalogue] = useState({ loadedProducts: null, positionScroll: 0 })
-  //hook que obtiene la pagina dondre estamos para manejar el estado de los productos guardados
-  useEffect(() => {
-    const { location } = window
-    const pathName = location.pathname.slice(0, 9)
-    if (pathName === '/product/' || pathName === '/catalogo') {
-    } else {
-      setReturnCatalogue({ loadedProducts: null, positionScroll: 0 })
-    }
-  }, [])
+  //se obtiene el pathname para guardar el estado returnCatalogue solo cuando sea catalogo o product
+  {
+    useRouter().pathname === '/product/[slug]' || '/catalogo'
+      ? ''
+      : () => setReturnCatalogue({ loadedProducts: null, positionScroll: 0 })
+  }
   return (
     <>
       <Head>
